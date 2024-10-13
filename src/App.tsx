3fddc20be257b19a7ee75cb1b7878c363ac7eb78
Christoffer Nguyen <christofferhoang@gmail.com>
2024-10-13 15:48:47 +0200

    change background color, and remove navbar

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,6 @@
 import React, { useEffect, useState } from 'react';
 import './App.css';
 import Loader from './components/Loader';
-import Navbar from "./components/Navbar";
-
 
 function App() {
   const [count, setCount] = useState(0);
@@ -17,27 +15,12 @@ function App() {
     return () => clearTimeout(timer);
   }, []);
 
-  const links = [
-    {
-      icon: "fa-brands fa-square-github fa-sm",
-      href: 'https://github.com',
-      key: 'github',
-    },
-    {
-      icon: 'fa-brands fa-square-linkedin fa-sm',
-      href: 'https://linkedin.com',
-      key: 'linkedin',
-    },
-    { icon: '', text: 'Contact', href: '/contact', key: 'contact' }, // Text link for Contact
-  ];
-
   return (
     <>
       {/* Loader remains in the DOM but is hidden with the loader--hidden class */}
       <Loader hidden={!isLoading} />
       {!isLoading && (
         <div>
-          <Navbar links={links} />
           <h1>Welcome to my website!</h1>
         </div>
       )}
